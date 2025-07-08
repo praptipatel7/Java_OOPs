8e84bfe3d4e015f41e6290030998d416062bc9df
praptipatel7 <patelprapti0737@gmail.com>
2025-07-08 14:58:01 +0530

    Java OOPs Day-2 Multiple Classes

--- demo2.java
+++ demo2.java
@@ -1,15 +1,29 @@
 import java.util.*;
-class demo2{
-    public static void main(String args[]){
+class DataClass {
+    int intValue;
+    float floatValue;
+    double doubleValue;
+    static String text = "This is static member.";
 
+    public DataClass() {
         Scanner sc = new Scanner(System.in);
-	    System.out.print("Enter the int value : ");
-        int i = sc.nextInt();
-        System.out.print("Enter the int float : ");
-        Float j = sc.nextFloat();
-        System.out.print("Enter the int double : ");
-        double k = sc.nextDouble();
+        System.out.print("Enter the int value: ");
+        intValue = sc.nextInt();
+        System.out.print("Enter the float value: ");
+        floatValue = sc.nextFloat();
+        System.out.print("Enter the double value: ");
+        doubleValue = sc.nextDouble();
+    }
+}
 
-        System.out.println("Int  : "+ i + "\n" +"Float : "+ j + "\n" +"Double : "+ k );
+class demo2 {
+    public static void main(String[] args) {
+        DataClass obj = new DataClass();
+        
+        // Access and print the three data members
+        System.out.println("Integer value: " + obj.intValue);
+        System.out.println("Float value: " + obj.floatValue);
+        System.out.println("Double value: " + obj.doubleValue);
+        System.out.println("String : " + DataClass.text);
     }
 }
